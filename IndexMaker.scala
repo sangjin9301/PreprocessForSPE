d1f9c4bf198fe77945c0ce95d9cd175098d48aca
sangjin9301 <Sangjin9301@gmail.com>
2018-02-09 17:58:58 +0900

    we have to implement Make index code

--- IndexMaker.scala
+++ IndexMaker.scala
@@ -1,22 +1,28 @@
+import java.sql.Timestamp
 import java.util
-import javafx.scene.chart.Chart
 
+import org.apache.commons.math3.stat.regression.SimpleRegression
 /**
   * Created by SANGJIN-NAM on 2018-01-18.
   */
 class IndexMaker {
 
+  //n일 이평 구하기 : [i-n] ~ [i]까지 종가의 평균을 구한다.
   def getAverageOf_n_Days(chart:util.LinkedList[Candle],n:Int):util.LinkedList[Double]={
-    // [i-n] ~ [i]까지 종가의 평균을 구한다.
     var MA_List = new util.LinkedList[Double]
-    // 0~(n-1) 까지는 무엇이 입력되어야 하는가.
+    var sum:Double = 0.
+
+    for( i <- 0 to n-1){
+      sum += chart.get(i).getEndPrice
+      MA_List.add(sum/(i+1))
+    }
+
     for( i <- n to chart.size()-1){
-      var sumOfEndPrice:Double = 0
+      sum = 0.
       for( j <- 0 to n){
-        sumOfEndPrice += chart.get(i-j).getEndPrice
+        sum += chart.get(i-j).getEndPrice
       }
-      var MA = sumOfEndPrice/n
-      MA_List.add(MA)
+      MA_List.add(sum/n)
     }
     return MA_List
   }
@@ -27,7 +33,8 @@ class IndexMaker {
       if(short.get(i-1)<long.get(i-1)){
         if(short.get(i)>long.get(i))return true
       }
-    }return false
+    }
+    return false
   }
 
   //단기가 장기를 하향으로 돌파
@@ -36,7 +43,8 @@ class IndexMaker {
       if(short.get(i-1)>long.get(i-1)){
         if(short.get(i)<long.get(i))return true
       }
-    }return false
+    }
+    return false
   }
 
   def getDisparity(chart: util.LinkedList[Candle], MA:util.LinkedList[Double]):util.LinkedList[Double]={
@@ -46,6 +54,286 @@ class IndexMaker {
     }
     return disparityList
   }
+
+  def getBollinger_upper(MA20_chart:util.LinkedList[Double]):util.LinkedList[Double]={
+    var mean = getMean(MA20_chart)
+    var sd = getStandardDeviation(mean, MA20_chart)
+    var upper_list = new util.LinkedList[Double]
+
+    MA20_chart.forEach( data => upper_list.add(data+(sd*2)))
+    return upper_list
+  }
+
+  def getBollinger_lower(MA20_chart:util.LinkedList[Double]):util.LinkedList[Double]={
+    var mean = getMean(MA20_chart)
+    var sd = getStandardDeviation(mean, MA20_chart)
+    var upper_list = new util.LinkedList[Double]
+
+    MA20_chart.forEach( data => upper_list.add(data-(sd*2)))
+    return upper_list
+  }
+
+  def getLRL(chart:util.LinkedList[Candle]):(Double,Double)={
+    var simpleRegression = new SimpleRegression
+    chart.forEach(candle => {
+      var date = candle.getTime.toString //ex: 20160515
+      var year = date.substring(0,3).toInt
+      var month = date.substring(4,5).toInt
+      var day = date.substring(6,7).toInt
+      var ts = new Timestamp(year,month,day,0,0,0,0)
+      simpleRegression.addData(ts.getTime,candle.getEndPrice)
+    })
+    var w:Double = simpleRegression.getSlope
+    var b:Double = simpleRegression.getIntercept
+    return (w,b) // LRL : ( EndPrice = w *Timestamp + b )
+  }
+
+  def getLRS(today_lrl:(Double,Double), yesterday_lrl:(Double,Double)):(Double,Double)={
+    return ((today_lrl._1+yesterday_lrl._1)/yesterday_lrl._1,(today_lrl._2+yesterday_lrl._2)/yesterday_lrl._2)
+  } // done : (당일LRL + 전일LRL)/전일LRL
+
+  def CCI(chart:util.LinkedList[Candle]):util.LinkedList[Double]={
+    var M_list = new util.LinkedList[Double]
+    var m_list = new util.LinkedList[Double]
+    var sub_Mm_list = new util.LinkedList[Double]
+    var d_list = new util.LinkedList[Double]
+    var cci_list = new util.LinkedList[Double]
+    var sum = 0.
+
+    // M = (고+저+종)/3
+    chart.forEach(candle => {
+      M_list.add((candle.getHighPrice + candle.getLowPrice + candle.getEndPrice) / 3)
+    })
+
+    // m = M의 20일 평균
+    for( i <- 0 to 19 ){
+      sum += M_list.get(i)
+      m_list.add(sum/(i+1))
+    }
+    sum = 0.
+    for( i <- 20 to M_list.size-1 ){
+      for( j <- (i-20) to i){
+        sum += M_list.get(j)
+      }
+      m_list.add(sum/20.)
+    }
+
+    // d = |M-m|의 20일 평균
+    for( i <- 0 to M_list.size-1 ){
+      sub_Mm_list.add(Math.abs(M_list.get(i) - m_list.get(i)))
+    }
+    sum = 0.
+    for( i <- 0 to 19 ){
+      sum += sub_Mm_list.get(i)
+      d_list.add(sum/(i+1))
+    }
+    sum = 0.
+    for( i <- 20 to sub_Mm_list.size-1){
+      for( j <- (i-20) to i){
+        sum += sub_Mm_list.get(j)
+      }
+      d_list.add(sum/20.)
+    }
+
+    for( i <- 0 to d_list.size-1){
+      var cii = ((M_list.get(i) - m_list.get(i))/(d_list.get(i))*0.015)
+      cci_list.add(cii)
+    }
+    return cci_list
+  }
+
+  def getMean(list:util.LinkedList[Double]):Double={
+    var sum = 0.
+    list.forEach( data => sum += data )
+    return sum/list.size
+  }
+
+  def getStandardDeviation(mean:Double, list:util.LinkedList[Double]):Double={
+    var sum = 0.
+    var diff = 0.
+
+    list.forEach( data => {
+      diff = data - mean
+      sum += diff * diff
+    })
+
+    return Math.sqrt(sum / (list.size-1))
+  }
+
+  //전환선
+  def getConversionLine(chart:util.LinkedList[Candle]):util.LinkedList[Double]={
+    var conversionLine = new util.LinkedList[Double]
+    for( i <- 0 to chart.size-1){
+      var list_9Days = new util.LinkedList[Double]
+      for( j <- 0 to 8){
+        try{
+          list_9Days.add(chart.get(i-j).getEndPrice)
+        }catch{
+          case e:NullPointerException => list_9Days.add(chart.get(i).getEndPrice)
+        }
+      }
+      var max = getMax(list_9Days)
+      var min = getMin(list_9Days)
+      conversionLine.add((max+min)/2)
+    }
+    return conversionLine
+  }
+
+  //기준선
+  def getStandardLine(chart:util.LinkedList[Candle]):util.LinkedList[Double]={
+    var standardLine = new util.LinkedList[Double]
+    for( i <- 0 to chart.size-1){
+      var list_26Days = new util.LinkedList[Double]
+      for( j <- 0 to 25){
+        try{
+          list_26Days.add(chart.get(i-j).getEndPrice)
+        }catch{
+          case e:NullPointerException => list_26Days.add(chart.get(i).getEndPrice)
+        }
+      }
+      var max = getMax(list_26Days)
+      var min = getMin(list_26Days)
+      standardLine.add((max+min)/2)
+    }
+    return standardLine
+  }
+
+  def TrailingSpan(standard:util.LinkedList[Double],conversion:util.LinkedList[Double]):util.LinkedList[Double]={
+    var span = new util.LinkedList[Double]
+    var trailingSpan = new util.LinkedList[Double]
+    for( i <- 0 to standard.size -1 ){
+      span.add((standard.get(i)+conversion.get(i))/2)
+    }
+    for( i <- 25 to span.size -1 ){
+      trailingSpan.add(span.get(i))
+    }
+    return trailingSpan
+  }
+
+  def LeadingSpan_1(standard:util.LinkedList[Double],conversion:util.LinkedList[Double]):util.LinkedList[Double]={
+    var span = new util.LinkedList[Double]
+    var leadingSpan_1 = new util.LinkedList[Double]
+    for( i <- 0 to standard.size -1 ){
+      span.add((standard.get(i)+conversion.get(i))/2)
+    }
+    for( i <- 0 to 24){
+      leadingSpan_1.add(0)
+    }
+    for( i <- 0 to span.size -1 ){
+      leadingSpan_1.add(span.get(i))
+    }
+    for( i <- 0 to 24){
+      leadingSpan_1.removeLast
+    }
+    return leadingSpan_1
+  }
+
+  def LeadingSpan_2(chart:util.LinkedList[Candle]):util.LinkedList[Double]={
+    var leadingSpan_2 = new util.LinkedList[Double]
+    var span = new util.LinkedList[Double]
+    var lowerThan52Index = new util.LinkedList[Double]
+    for( i <- 0 to 50){
+      lowerThan52Index.add(chart.get(i).getEndPrice)
+      span.add((getMax(lowerThan52Index)+getMin(lowerThan52Index))/2)
+    }
+    for( i <- 51 to chart.size()-1){
+      var list = new util.LinkedList[Double]
+      for( j <- 0 to 51){
+        list.add(chart.get(i-j).getEndPrice)
+      }
+      span.add((getMax(list)+getMin(list))/2)
+    }
+    for( i <- 0 to 24){
+      leadingSpan_2.add(0)
+    }
+    for( i <- 0 to span.size-1 ){
+      leadingSpan_2.add(span.get(i))
+    }
+    for( i <- 0 to 24){
+      leadingSpan_2.removeLast
+    }
+    return leadingSpan_2
+  }
+
+  def getMax(list:util.LinkedList[Double]):Double={
+    var max = 0.
+    list.forEach( x => { if( max < x )max=x})
+    return max
+  }
+
+  def getMin(list:util.LinkedList[Double]):Double={
+    var min = 0.
+    list.forEach( x => { if( min < x )min=x})
+    return min
+  }
+
+  def getEMA_1(chart:util.LinkedList[Candle]):util.LinkedList[Double]={
+    var EMA_List = new util.LinkedList[Double]
+    var sum:Double = 0.
+
+    for( i <- 0 to 24){
+      sum += chart.get(i).getEndPrice
+      EMA_List.add(sum/(i+1))
+    }
+
+    for( i <- 25 to chart.size()-1){
+      sum = 0.
+      for( j <- 0 to 25){
+        sum += chart.get(i-j).getEndPrice
+      }
+      EMA_List.add(sum/25)
+    }
+    return EMA_List
+  }
+
+  def getEMA_2(chart:util.LinkedList[Double]):util.LinkedList[Double]={
+    var EMA_List2 = new util.LinkedList[Double]
+    var sum:Double = 0.
+
+    for( i <- 0 to 24){
+    sum += chart.get(i)
+    EMA_List2.add(sum/(i+1))
+  }
+
+    for( i <- 25 to chart.size()-1){
+    sum = 0.
+    for( j <- 0 to 25){
+    sum += chart.get(i-j)
+  }
+    EMA_List2.add(sum/25)
+  }
+    return EMA_List2
+  }
+
+  def getEMA_3(chart:util.LinkedList[Double]):util.LinkedList[Double]={
+    var EMA_List3 = new util.LinkedList[Double]
+    var sum:Double = 0.
+
+    for( i <- 0 to 24){
+      sum += chart.get(i)
+      EMA_List3.add(sum/(i+1))
+    }
+
+    for( i <- 25 to chart.size()-1){
+      sum = 0.
+      for( j <- 0 to 25){
+        sum += chart.get(i-j)
+      }
+      EMA_List3.add(sum/25)
+    }
+    return EMA_List3
+  }
+  //todo - Have to implements
+  def getTRIX(chart:util.LinkedList[Double]):util.LinkedList[Double]
+  //todo - Have to implements
+  def getTRIX_Sig(chart:util.LinkedList[Double]):util.LinkedList[Double]
+
+  //todo - Have to implements
+  def get_perK(chart:util.LinkedList[Candle]):util.LinkedList[Double]
+  //todo - Have to implements
+  def get_perD(chart:util.LinkedList[Double]):util.LinkedList[Double]
+  //todo - Have to implements
+  def get_Slow_perK(chart:util.LinkedList[Double]):util.LinkedList[Double]
 
 
 

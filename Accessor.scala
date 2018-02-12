77635bab1ecf2d5de3823d71e52d731de070b838
sangjin9301 <Sangjin9301@gmail.com>
2018-02-12 18:57:18 +0900

    todo - Candle set/get method (거래량, 거래대금, 전일대비)

--- Accessor.scala
+++ Accessor.scala
@@ -1,18 +1,58 @@
+import java.io.{BufferedReader, File, FileReader, FileWriter}
+
+import scala.collection.mutable.ArrayBuffer
+import org.apache.commons.io.LineIterator
 /**
-  * Created by SANGJIN-NAM on 2018-01-18.
+  * Created by SANGJIN-NAM on 2018-02-12
   */
+
+
+/*
+ * todo
+ * 1. 파일읽기
+ * 2. 파라미터로 candle 생성하기
+ * 3. candleChart 만들기
+ *
+ */
 class Accessor(path:String) {
 
-  var InputPath = path
+  val splitBy = ","
+
+  def getData(path: String): Array[Array[String]] = {
+    var arr = new ArrayBuffer[Array[String]]
+    var bufReader: BufferedReader = null
+    bufReader = new BufferedReader(new FileReader(path))
+    val iter = new LineIterator(bufReader)
+
+    while (iter.hasNext) {
+      arr += iter.nextLine().split(splitBy)
+    }
+    arr.remove(0)
+    return arr.toArray
+  }
+
+  def toCandle(arr: Array[Array[String]]): Array[Candle] = {
+    var candle_list = new ArrayBuffer[Candle]
+
+    //todo Implements get/set method
+
+    arr.foreach(line => {
+      var candle = new Candle
+      candle.setTime(line(0))
+      candle.setStartPrice(line(1))
+      candle.setHighPrice(line(2).toDouble)
+      candle.setLowPrice(line(3).toInt)
+      candle.setEndPrice(line(4).toInt)
+      //거래량 candle.set(line(5))
+      //거래대금 candle.set(line(6))
+      candle.setDirection(line(7))
+      //전일대비
+      candle.setSolidity(line(9))
+      candle_list += candle
+    })
+    return candle_list.toArray
+  }
 
 
-
-  /*
-   *
-   * 1. 파일읽기
-   * 2. 파라미터로 candle 생성하기
-   * 3. candleChart 만들기
-   *
-   */
 
 }
